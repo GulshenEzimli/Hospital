fcb8e75b4ae80ce86db3c1abffaf1e477fa6c656
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-04-05 22:50:03 +0400

    App : Add DroopDownEmployeesCommand

---
+++ HospitalManagement/Commands/Dashboard/DropDownEmloyeesCommand.cs
@@ -0,0 +1,22 @@
+﻿using HospitalManagement.ViewModels.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Commands.Dashboard
+{
+    public class DropDownEmloyeesCommand : BaseCommand
+    {
+        private readonly DashboardViewModel _viewModel;
+        public DropDownEmloyeesCommand(DashboardViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            _viewModel.EmployeeSituation = !(_viewModel.EmployeeSituation);
+        }
+    }
+}
--- HospitalManagement/ViewModels/Windows/DashboardViewModel.cs
+++ HospitalManagement/ViewModels/Windows/DashboardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalManagement.Commands.Dashboard;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,24 @@ using System.Threading.Tasks;
 
 namespace HospitalManagement.ViewModels.Windows
 {
-    public class DashboardViewModel
+    public class DashboardViewModel : BaseViewModel
     {
         public DashboardViewModel()
         {
             
         }
+
+        private bool _employeeSituation = false;
+        public bool EmployeeSituation
+        {
+            get { return _employeeSituation; }
+            set
+            {
+                _employeeSituation = value;
+                OnPropertyChanged(nameof(EmployeeSituation));
+            }
+        }
+
+        public DropDownEmloyeesCommand DropDown => new DropDownEmloyeesCommand(this);
     }
 }
