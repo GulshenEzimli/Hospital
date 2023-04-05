549cbd94f81f56c5d145cc992abf44cd1fe6f70a
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-04-05 17:43:46 +0400

    Add project files.

---
+++ HospitalManagement/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using HospitalManagement.ViewModels.Windows;
+using HospitalManagement.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HospitalManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            DashboardWindow dashboardWindow = new DashboardWindow();
+            DashboardViewModel viewModel = new DashboardViewModel();
+
+            dashboardWindow.DataContext = viewModel;
+            MainWindow = dashboardWindow;
+            MainWindow.Show();
+        }
+    }
+}
---
+++ HospitalManagement/ViewModels/Windows/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.ViewModels.Windows
+{
+    public class DashboardViewModel
+    {
+        public DashboardViewModel()
+        {
+            
+        }
+    }
+}
