fa7ad5786bfafeacc48675cb3717b38a41c3bdbc
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2023-04-08 17:26:37 +0400

    Save Receptionist command

--- HospitalManagement/Commands/Receptionists/SaveReceptionistCommand.cs
+++ HospitalManagement/Commands/Receptionists/SaveReceptionistCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HospitalManagement.Enums;
+using HospitalManagement.ViewModels.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace HospitalManagement.Commands.Receptionists
 {
-    internal class SaveReceptionistCommand
+    internal class SaveReceptionistCommand:BaseCommand
     {
+        private readonly ReceptionistViewModel _receptionistViewModel;
+        public SaveReceptionistCommand(ReceptionistViewModel receptionistViewModel)
+        {
+            _receptionistViewModel = receptionistViewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            //TO DO ...
+            _receptionistViewModel.CurrentSituation = (int)Situations.NORMAL;
+        }
     }
 }
