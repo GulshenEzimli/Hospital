9dc589d34e6d5027812b91ff7fb07719a033090d
SureyyaKamilova <sureyyakamilovaa@gmail.com>
2023-04-09 14:13:03 +0400

    App:Create QueuesControl and add commands

---
+++ HospitalManagement/Commands/Queues/AddQueueCommand.cs
@@ -0,0 +1,24 @@
+﻿using HospitalManagement.Enums;
+using HospitalManagement.ViewModels.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Commands.Queues
+{
+    public class AddQueueCommand : BaseCommand
+    {
+        private readonly QueuesViewModel _queueViewModel;
+
+        public AddQueueCommand(QueuesViewModel queueViewModel)
+        {
+            _queueViewModel = queueViewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            _queueViewModel.CurrentSituation = (int)Situations.ADD;
+        }
+    }
+}
---
+++ HospitalManagement/Commands/Queues/DeleteQueueCommand.cs
@@ -0,0 +1,22 @@
+﻿using HospitalManagement.ViewModels.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Commands.Queues
+{
+    public class DeleteQueueCommand : BaseCommand
+    {
+        private readonly QueuesViewModel _queueViewModel;
+        public DeleteQueueCommand(QueuesViewModel queueViewModel)
+        {
+            _queueViewModel = queueViewModel;
+        }
+        public override void Execute(object parameter)
+        {
+
+        }
+    }
+}
