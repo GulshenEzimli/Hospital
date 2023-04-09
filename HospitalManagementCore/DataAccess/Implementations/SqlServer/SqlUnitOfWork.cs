21ce81e2d6dbc74787f8c607f5d680aad5fb9e6a
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-10 01:31:22 +0400

    Modifie IUnitOfWork interface and add UnitOfWork

---
+++ HospitalManagementCore/DataAccess/Implementations/SqlServer/SqlUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using HospitalManagementCore.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementCore.DataAccess.Implementations.SqlServer
+{
+    public class SqlUnitOfWork : IUnitOfWork
+    {
+        private readonly string _connectionString;
+        public SqlUnitOfWork(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDoctorRepository DoctorRepository => new SqlDoctorRepository(_connectionString);
+    }
+}
--- HospitalManagementCore/DataAccess/Interfaces/IUnitOfWork.cs
+++ HospitalManagementCore/DataAccess/Interfaces/IUnitOfWork.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace HospitalManagementCore.DataAccess.Interfaces
 {
-    internal interface IUnitOfWork
+    public interface IUnitOfWork
     {
         IDoctorRepository DoctorRepository { get; }
     }
