ea4998b9051f05499b02bd2a05a33d6bd108ff34
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-04-12 00:23:06 +0400

    App : Update OpenOtherEmployeeCommand

--- HospitalManagement/Commands/Dashboard/OpenOtherEmployeesCommand.cs
+++ HospitalManagement/Commands/Dashboard/OpenOtherEmployeesCommand.cs
@@ -1,6 +1,9 @@
-﻿using HospitalManagement.ViewModels.UserControls;
+﻿using HospitalManagement.Mappers.Interfaces;
+using HospitalManagement.Models;
+using HospitalManagement.ViewModels.UserControls;
 using HospitalManagement.ViewModels.Windows;
 using HospitalManagement.Views.UserControls;
+using HospitalManagementCore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +15,25 @@ namespace HospitalManagement.Commands.Dashboard
     public class OpenOtherEmployeesCommand : BaseCommand
     {
         private readonly DashboardViewModel _viewModel;
-        public OpenOtherEmployeesCommand(DashboardViewModel viewModel)
+        private readonly IOtherEmployeeMapper _otherEmployeeMapper;
+        public OpenOtherEmployeesCommand(DashboardViewModel viewModel, IOtherEmployeeMapper otherEmployeeMapper)
         {
             _viewModel = viewModel;
+            _otherEmployeeMapper = otherEmployeeMapper;
         }
         public override void Execute(object parameter)
         {
             OtherEmployeesViewModel otherEmployeesViewModel = new OtherEmployeesViewModel(_viewModel.Db);
+
+            List<OtherEmployee> otherEmployees = _viewModel.Db.OtherEmployeeRepository.Get();
+            int no = 1;
+            foreach (var otherEmployee in otherEmployees)
+            {
+                OtherEmployeeModel otherEmployeeModel = _otherEmployeeMapper.Map(otherEmployee);
+                otherEmployeeModel.No = no++;
+                otherEmployeesViewModel.OtherEmployeeValues.Add(otherEmployeeModel);
+            }
+
             OtherEmployeesControl otherEmployeesControl = new OtherEmployeesControl();
 
             otherEmployeesControl.DataContext = otherEmployeesViewModel;
