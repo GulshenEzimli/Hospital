e07eb32b92f4ac020cb6ae83e83cf039ab5c8173
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-04-12 23:47:51 +0400

    Modifid PatientProcedureViewModel and PatientProcedureControl

--- HospitalManagement/ViewModels/UserControls/PatientProcedureViewModel.cs
+++ HospitalManagement/ViewModels/UserControls/PatientProcedureViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using HospitalManagement.Commands.PatientProcedures;
 using HospitalManagement.Enums;
+using HospitalManagement.Models;
 using HospitalManagementCore.DataAccess.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,20 @@ namespace HospitalManagement.ViewModels.UserControls
                 OnPropertyChanged(nameof(CurrentSituation));
             }
         }
+
+        private PatientProcedureModel _patientProcedureModel;
+        public PatientProcedureModel PatientProcedureModel
+        {
+            get => _patientProcedureModel;
+            set
+            {
+                _patientProcedureModel = value;
+                OnPropertyChanged(nameof(PatientProcedure));
+            }
+        }
+
+        private List<PatientProcedureModel> _patientProcedureValues;
+        public List<PatientProcedureModel> PatientProcedureValues => _patientProcedureValues ?? (_patientProcedureValues = new List<PatientProcedureModel>());
 
         public AddPatientProcedureCommand Add => new AddPatientProcedureCommand(this);
         public DeletePatientProcedureCommand Delete => new DeletePatientProcedureCommand(this);
