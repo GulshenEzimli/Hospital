236ea9679664c5e35cfa3a70e54c3fad6b3ddea6
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-12 18:51:45 +0400

    Add SqlPositionRepository

---
+++ HospitalManagementCore/DataAccess/Implementations/SqlServer/SqlPositionRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementCore.DataAccess.Implementations.SqlServer
+{
+    internal class SqlPositionRepository
+    {
+    }
+}
---
+++ HospitalManagementCore/DataAccess/Interfaces/IPositionRepoitory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementCore.DataAccess.Interfaces
+{
+    internal interface IPositionRepoitory
+    {
+    }
+}
