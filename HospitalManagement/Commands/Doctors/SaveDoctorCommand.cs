978b3c65e925c1245a2190fc3df401fd8839cd0c
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-15 17:04:53 +0400

    Mapping from DoctorModel to Doctor entity

--- HospitalManagement/Commands/Doctors/SaveDoctorCommand.cs
+++ HospitalManagement/Commands/Doctors/SaveDoctorCommand.cs
@@ -1,5 +1,7 @@
 ﻿using HospitalManagement.Enums;
+using HospitalManagement.Mappers.Interfaces;
 using HospitalManagement.ViewModels.UserControls;
+using HospitalManagementCore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +13,32 @@ namespace HospitalManagement.Commands.Doctors
     public class SaveDoctorCommand : BaseCommand
     {
         private readonly DoctorsViewModel _doctorsViewModel;
-        public SaveDoctorCommand(DoctorsViewModel doctorsViewModel)
+        private readonly IDoctorMapper _doctorMapper;
+        public SaveDoctorCommand(DoctorsViewModel doctorsViewModel, IDoctorMapper doctorMapper)
         {
             _doctorsViewModel = doctorsViewModel;
+            _doctorMapper = doctorMapper;
         }
 
         public override void Execute(object parameter)
         {
-            //TO DO : implement save
+            Doctor doctor = _doctorMapper.Map(_doctorsViewModel.CurrentValue);
+            doctor.CreationDate = DateTime.Now;
+            doctor.ModifiedDate = DateTime.Now;
+            doctor.IsDelete = false;
+            doctor.Creator = new Admin() { Id = 1 };
+            doctor.Modifier = new Admin() { Id = 1 };
+            if (doctor.Id==0)
+            {
+                _doctorsViewModel.Db.DoctorRepository.Insert(doctor);
+            }
+            else
+            {
+                _doctorsViewModel.Db.DoctorRepository.Update(doctor);
+            }
 
-            _doctorsViewModel.CurrentSituation = Situations.NORMAL;
+
+            _doctorsViewModel.SetDefaultValues();
         }
     }
 }
