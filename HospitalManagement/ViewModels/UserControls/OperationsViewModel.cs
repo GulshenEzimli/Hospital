7ee9391b3c0b71dc16a9147aca5655be4122f8fd
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-16 23:03:25 +0400

    Add OperationsViewModel

---
+++ HospitalManagement/ViewModels/UserControls/OperationsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using HospitalManagement.Enums;
+using HospitalManagement.Models;
+using HospitalManagement.Views.Components;
+using HospitalManagementCore.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.ViewModels.UserControls
+{
+    public class OperationsViewModel : BaseControlViewModel
+    {
+        public OperationsViewModel(IUnitOfWork unitOfWork, ErrorDialog errorDialog) : base(unitOfWork, errorDialog)
+        {
+            SetDefaultValues();
+        }
+        public override string Header => "Operations";
+
+        private Situations _currentSituation;
+        public Situations CurrentSituation
+        {
+            get => _currentSituation;
+            set
+            {
+                _currentSituation = value;
+                OnPropertyChanged(nameof(CurrentSituation));
+            }
+        }
+        public void SetDefaultValues()
+        {
+            CurrentSituation = Situations.NORMAL;
+        }
+    }
+}
