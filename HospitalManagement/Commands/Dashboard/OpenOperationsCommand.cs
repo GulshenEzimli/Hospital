20a3a06f35dcb937a492ee8b8f45a88483a35000
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-16 23:03:39 +0400

    Add OpenOperationsCommand

---
+++ HospitalManagement/Commands/Dashboard/OpenOperationsCommand.cs
@@ -0,0 +1,33 @@
+﻿using HospitalManagement.Mappers.Implementations;
+using HospitalManagement.Mappers.Interfaces;
+using HospitalManagement.Models;
+using HospitalManagement.ViewModels.UserControls;
+using HospitalManagement.ViewModels.Windows;
+using HospitalManagement.Views.UserControls;
+using HospitalManagementCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Commands.Dashboard
+{
+    public class OpenOperationsCommand : BaseCommand
+    {
+        private readonly DashboardViewModel _viewModel;
+        public OpenOperationsCommand(DashboardViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            OperationControl operationControl = new OperationControl();
+            OperationsViewModel operationsViewModel = new OperationsViewModel(_viewModel.Db, operationControl.ErrorDialog);
+
+            operationControl.DataContext = operationsViewModel;
+            _viewModel.CenterGrid.Children.Clear();
+            _viewModel.CenterGrid.Children.Add(operationControl);
+        }
+    }
+}
