fb68b9a76fa374f7e9f632418dfab8118a211a00
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-17 22:59:36 +0400

    Add IOperationDoctorMapper and OperationDoctorMapper

---
+++ HospitalManagement/Mappers/Implementations/OperationDoctorMapper.cs
@@ -0,0 +1,28 @@
+﻿using HospitalManagement.Mappers.Interfaces;
+using HospitalManagement.Models;
+using HospitalManagementCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Mappers.Implementations
+{
+    public class OperationDoctorMapper : IOperationDoctorMapper
+    {
+        public OperationDoctorModel Map(OperationDoctor operationDoctor)
+        {
+            OperationDoctorModel operationDoctorModel = new OperationDoctorModel();
+            operationDoctorModel.DoctorName = operationDoctor.Doctor.FirstName;
+            operationDoctorModel.DoctorSurname = operationDoctor.Doctor.LastName;
+            operationDoctorModel.DoctorPIN = operationDoctor.Doctor.PIN;
+            return operationDoctorModel;
+        }
+
+        public OperationDoctor Map(OperationDoctorModel operationDoctorModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HospitalManagement/Mappers/Interfaces/IOperationDoctorMapper.cs
@@ -0,0 +1,16 @@
+﻿using HospitalManagement.Models;
+using HospitalManagementCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Mappers.Interfaces
+{
+    public interface IOperationDoctorMapper
+    {
+        OperationDoctorModel Map(OperationDoctor operationDoctor);
+        OperationDoctor Map(OperationDoctorModel operationDoctorModel);
+    }
+}
