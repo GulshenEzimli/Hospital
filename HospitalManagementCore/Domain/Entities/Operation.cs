7faaffbf11f8a92e1e73448d0a9127d9a3fa8d31
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-17 13:54:22 +0400

    Add Operation, OperationDoctor, OperationNurse entity

---
+++ HospitalManagementCore/Domain/Entities/Operation.cs
@@ -0,0 +1,19 @@
+﻿using HospitalManagementCore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementCore.Domain.Entities
+{
+    public class Operation : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime OperationDate { get; set; }
+        public decimal OperationCost { get; set; }
+        public string OperationReason { get; set; }
+        public int PatientId => Patient?.Id ?? 0;
+        public int RoomId => Room?.Id ?? 0;
+        public Patient Patient { get; set; }
+        public Room Room { get; set; }
+    }
+}
---
+++ HospitalManagementCore/Domain/Entities/OperationNurse.cs
@@ -0,0 +1,16 @@
+﻿using HospitalManagementCore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementCore.Domain.Entities
+{
+    public class OperationNurse : IEntity
+    {
+        public int Id { get; set; }
+        public int OperationId => Operation?.Id ?? 0;
+        public int NurseId => Nurse?.Id ?? 0;
+        public Operation Operation { get; set; }
+        public Nurse Nurse { get; set; }
+    }
+}
