a9e5b65be6333cec56a0145b9a402f92a9318770
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-17 23:00:36 +0400

    Add OperationDoctorModel and OperationNurseModel

---
+++ HospitalManagement/Models/OperationDoctorModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Models
+{
+    public class OperationDoctorModel
+    {
+        public int Id { get; set; }
+        public string DoctorName { get; set; }
+        public string DoctorSurname { get; set; }
+        public string DoctorPIN { get; set; }
+    }
+}
---
+++ HospitalManagement/Models/OperationNurseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Models
+{
+    public class OperationNurseModel
+    {
+        public int Id { get; set; }
+        public string NurseName { get; set; }
+        public string NurseSurname { get; set; }
+        public string NursePIN { get; set; }
+    }
+}
