54e387eaec91fb4498bc302a86f1781120b3954b
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-17 23:00:07 +0400

    Add IOperationNurseMapper and OperationNurseMapper

---
+++ HospitalManagement/Mappers/Implementations/OperationNurseMapper.cs
@@ -0,0 +1,28 @@
+﻿using HospitalManagement.Mappers.Interfaces;
+using HospitalManagement.Models;
+using HospitalManagementCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Mappers.Implementations
+{
+    public class OperationNurseMapper : IOperationNurseMapper
+    {
+        public OperationNurseModel Map(OperationNurse operationNurse)
+        {
+            OperationNurseModel operationNurseModel = new OperationNurseModel();
+            operationNurseModel.NurseName = operationNurse.Nurse.FirstName;
+            operationNurseModel.NurseSurname = operationNurse.Nurse.LastName;
+            operationNurseModel.NursePIN = operationNurse.Nurse.PIN;
+            return operationNurseModel;
+        }
+
+        public OperationNurse Map(OperationNurseModel operationNurseModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
