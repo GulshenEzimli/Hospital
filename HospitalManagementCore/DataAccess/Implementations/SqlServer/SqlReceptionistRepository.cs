cae38c83a665093d74a6118d9754c86b69f16dd4
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2023-04-22 20:10:46 +0400

    GetReceptionist and Get Method

--- HospitalManagementCore/DataAccess/Implementations/SqlServer/SqlReceptionistRepository.cs
+++ HospitalManagementCore/DataAccess/Implementations/SqlServer/SqlReceptionistRepository.cs
@@ -1,5 +1,6 @@
 ﻿using HospitalManagementCore.DataAccess.Interfaces;
 using HospitalManagementCore.Domain.Entities;
+using HospitalManagementCore.Utils;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -32,7 +33,25 @@ namespace HospitalManagementCore.DataAccess.Implementations.SqlServer
 
         public List<Receptionist> Get()
         {
-            throw new NotImplementedException();
+            using(SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string cmdText = @"";
+                using(SqlCommand command= new SqlCommand( cmdText,connection))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    List<Receptionist> receptionists = new List<Receptionist>();
+
+                    while (reader.Read())
+                    {
+                        Receptionist receptionist = GetReceptionist(reader);
+                        receptionists.Add(receptionist);
+                    }
+                    return receptionists;
+                    
+                    // yadindan cixir bax 
+                }
+            }
         }
 
         public Receptionist GetById(int id)
@@ -48,6 +67,43 @@ namespace HospitalManagementCore.DataAccess.Implementations.SqlServer
         public bool Update(Receptionist entity)
         {
             throw new NotImplementedException();
+        }
+
+        private Receptionist GetReceptionist(SqlDataReader reader)
+        {
+            Receptionist receptionist = new Receptionist();
+
+            receptionist.Id = reader.GetInt32("ReceptionistId");
+            receptionist.FirstName= reader.GetString("FirstName");
+            receptionist.LastName = reader.GetString("LastName");
+            receptionist.Gender = reader.GetBoolean("Gender");
+            receptionist.PIN = reader.GetString("PIN");
+            receptionist.Email=reader.GetString("Email");
+            receptionist.PhoneNumber = reader.GetString("PhoneNumber");
+            receptionist.BirthDate = reader.GetDateTime("BirthDate");
+            receptionist.Salary = reader.GetDecimal("Salary");
+            receptionist.CreationDate = reader.GetDateTime("CreationDate");
+            receptionist.ModifierDate = reader.GetDateTime("ModifierDate");
+            receptionist.IsDelete = reader.GetBoolean("IsDeleted");
+
+
+            receptionist.Job = new Job()
+            {
+                Id = reader.GetInt32("JobId"),
+                Name = reader.GetString("JobName")
+            };
+
+            receptionist.Creator = new Admin()
+            {
+                Id = reader.GetInt32("CreatorId")
+            };
+            receptionist.Modifier = new Admin()
+            {
+                Id = reader.GetInt32("ModifierId")
+            };
+            
+            return receptionist;
+          
         }
     }
 }
