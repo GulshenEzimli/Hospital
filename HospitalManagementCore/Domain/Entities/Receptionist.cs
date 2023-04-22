1926edb40be52c49516782032f9495d99b3aa84f
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2023-04-22 18:12:18 +0400

    Receptionist Entites

--- HospitalManagementCore/Domain/Entities/Receptionist.cs
+++ HospitalManagementCore/Domain/Entities/Receptionist.cs
@@ -7,7 +7,7 @@ namespace HospitalManagementCore.Domain.Entities
 {
     public class Receptionist : IEntity
     {
-        // demeli birinci kateqoriyalara ayirim daha rahat anlasilsin id hec yazdiq 
+
         public int Id { get; set; }
 
         // Receptionist main data
@@ -15,5 +15,31 @@ namespace HospitalManagementCore.Domain.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PIN { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Salary { get; set; }
+
+
+        public DateTime CreationDate { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime ModifierDate { get; set; }
+
+
+        public int CreatorId => Creator?.Id ?? 0;
+        public int ModifierId => Modifier?.Id ?? 0;
+
+
+        public Admin Creator { get; set; }
+        public Admin Modifier { get; set; }
+
+
+        public int JobId => Job?.Id ?? 0;
+
+        public Job Job { get; set; }
+
+        
+
     }
 }
