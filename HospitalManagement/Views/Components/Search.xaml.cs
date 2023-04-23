68c638e5e17b4d15b484e32e5eceed9b16edb1c8
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-24 02:38:44 +0400

    Add Search components

---
+++ HospitalManagement/Views/Components/Search.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HospitalManagement.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for Search.xaml
+    /// </summary>
+    public partial class Search : UserControl
+    {
+        public Search()
+        {
+            InitializeComponent();
+        }
+    }
+}
