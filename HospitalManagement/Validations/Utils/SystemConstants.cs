6ece1d8faa4ecf09aaf4bb3cc4e817d0279c0367
Umud-Iskenderov <iskenderovumud.2003@gmail.com>
2023-04-24 02:37:53 +0400

    Add SystemConstants

---
+++ HospitalManagement/Validations/Utils/SystemConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Validations.Utils
+{
+    public class SystemConstants
+    {
+        public const string DateDisplayFormat = "dd.MM.yyyy";
+    }
+}
