c8296daab6c26fb3eda01da72334bc1ac6ae6c64
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2023-04-24 18:57:30 +0400

    Receptionist Model

--- HospitalManagement/Models/ReceptionistModel.cs
+++ HospitalManagement/Models/ReceptionistModel.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace HospitalManagement.Models
 {
-    internal class ReceptionistModel
+    public class ReceptionistModel
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int No { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PIN { get; set; }
+        public string JobName { get; set; }
+        public string DepartmentName { get; set; }
+        public decimal Salary { get; set; }
+        private bool[] _gender = { false, false };
+        public bool[] Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        public string GenderValue
+        {
+            get
+            {
+                return _gender[0] ? "Kişi" : "Qadın";
+            }
+        }
+
+        public ReceptionistModel Clone()
+        {
+            return new ReceptionistModel()
+            {
+                Id = Id,
+                FirstName = FirstName,
+                LastName = LastName,
+                BirthDate = BirthDate,
+                PhoneNumber = PhoneNumber,
+                Email = Email,
+                PIN = PIN,
+                JobName = JobName,
+                DepartmentName = DepartmentName,
+                Salary = Salary,
+                Gender = Gender,
+
+            };
+        }
+
     }
 }
