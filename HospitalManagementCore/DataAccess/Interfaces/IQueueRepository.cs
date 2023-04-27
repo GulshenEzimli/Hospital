fae34680a9599a05c7790ed7bc1626749b091262
SureyyaKamilova <sureyyakamilovaa@gmail.com>
2023-04-27 19:50:18 +0400

    Add QueueRepository

---
+++ HospitalManagementCore/DataAccess/Interfaces/IQueueRepository.cs
@@ -0,0 +1,11 @@
+﻿using HospitalManagementCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementCore.DataAccess.Interfaces
+{
+    public interface IQueueRepository:IEntityRepository<Queue>
+    {
+    }
+}
---
+++ HospitalManagementCore/Domain/Entities/Queue.cs
@@ -0,0 +1,16 @@
+﻿using HospitalManagementCore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementCore.Domain.Entities
+{
+    public class Queue : IEntity
+    {
+        public int Id { get; set; }
+        public Doctor DoctorId { get; set; }
+        public Patient PatientId { get; set; }
+        public int QueueNumber { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
