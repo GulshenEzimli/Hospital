04fc1ae6e1dc9df970495583b01bf7be760c13da
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-05-01 00:47:37 +0400

    Modify  display to objects in NurseControl,OtherEmployeeControl,PatientProcedureControl

--- HospitalManagement/Validations/PatientProcedureValidation.cs
+++ HospitalManagement/Validations/PatientProcedureValidation.cs
@@ -12,21 +12,26 @@ namespace HospitalManagement.Validations
     {
         public static bool IsValid(PatientProcedureModel patientProcedureModel, out string message)
         {
-            if (string.IsNullOrEmpty(patientProcedureModel.DisplayPatient))
+            if (string.IsNullOrEmpty(patientProcedureModel.Patient.DisplayPatient))
             {
                 message = ValidationMessageProvider.GetRequiredMessage("Patient value");
                 return false;
             }
 
-            if (string.IsNullOrEmpty(patientProcedureModel.DisplayDoctor))
+            if (string.IsNullOrEmpty(patientProcedureModel.Doctor.DisplayDoctor))
             {
                 message = ValidationMessageProvider.GetRequiredMessage("Doctor value");
                 return false;
             }
 
-            if (string.IsNullOrEmpty(patientProcedureModel.DisplayNurse))
+            if (string.IsNullOrEmpty(patientProcedureModel.Nurse.DisplayNurse))
             {
                 message = ValidationMessageProvider.GetRequiredMessage("Nurse value");
+                return false;
+            }
+            if (string.IsNullOrEmpty(patientProcedureModel.Procedure.DisplayProcedure))
+            {
+                message = ValidationMessageProvider.GetRequiredMessage("Procedure value");
                 return false;
             }
 
