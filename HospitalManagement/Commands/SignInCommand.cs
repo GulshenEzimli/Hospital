e52117e307a2a5bfb2369bfee3efa808738d2eac
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-05-07 00:04:18 +0400

    Add MysecurityHeper class to compute passwordhash and SignInCommand

---
+++ HospitalManagement/Commands/SignInCommand.cs
@@ -0,0 +1,53 @@
+﻿using HospitalManagement.Mappers.Interfaces;
+using HospitalManagement.Services.Interfaces;
+using HospitalManagement.ViewModels.Windows;
+using HospitalManagement.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HospitalManagement.Commands
+{
+    public class SignInCommand : BaseCommand
+    {
+        private readonly LoginViewModel _loginViewModel;
+        private readonly IAdminService _adminService;
+        private readonly IServiceUnitOfWork _serviceUnitOfWork;
+        private readonly IPatientMapper _patientMapper;
+        private readonly IProcedureMapper _procedureMapper;
+        private readonly IQueueMapper _queueMapper;
+        public SignInCommand(LoginViewModel loginViewModel, IAdminService adminService, 
+                                IServiceUnitOfWork serviceUnitOfWork,IPatientMapper patientMapper,
+                                IProcedureMapper procedureMapper,IQueueMapper queueMapper)
+        {
+            _loginViewModel = loginViewModel;
+            _adminService = adminService;
+            _serviceUnitOfWork = serviceUnitOfWork;
+            _patientMapper = patientMapper;
+            _procedureMapper = procedureMapper;
+            _queueMapper = queueMapper;
+        }
+        public override void Execute(object parameter)
+        {
+            var passwordBox = parameter as PasswordBox;
+            var passWord = passwordBox.Password;
+
+            var authorizationSuccess = _adminService.Authorize(_loginViewModel.Username, passWord);
+            if(authorizationSuccess)
+            {
+                DashboardWindow dashboardWindow = new DashboardWindow();
+                DashboardViewModel dashboardViewModel = new DashboardViewModel(_serviceUnitOfWork,_patientMapper,_procedureMapper, _queueMapper);
+
+                dashboardViewModel.CenterGrid = dashboardWindow.grdCenter;
+                dashboardWindow.DataContext = dashboardViewModel;
+
+                dashboardWindow.Show();
+                _loginViewModel.LoginPage.Close();
+            }
+            else _loginViewModel.ErrorVisibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
---
+++ HospitalManagementCore/Utils/MySecurityHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HospitalManagementCore.Utils
+{
+    public static class MySecurityHelper
+    {
+        public static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+
+                foreach (var t in bytes)
+                {
+                    builder.Append(t.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
